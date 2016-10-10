787bde251347a4e6a642ecb4b47073a02c4e5eb2
Christian Rodemeyer <christian.rodemeyer@tado.com>
2016-10-10 09:22:39 +0200

    add line to chart

--- index.js
+++ index.js
@@ -5,18 +5,30 @@ var margin = {top:10, bottom:50, left:50, right:30};
 var width = chart.node().clientWidth - margin.left - margin.right;
 var height = chart.node().clientHeight - margin.top - margin.bottom;
 
-var scaleX = d3.scaleLinear().domain([0, 10]).range([0, width]);
-var scaleY = d3.scaleLinear().domain([0, 10]).range([height, 0]);
+var x = d3.scaleTime()
+    .domain([data[0][0], data[data.length-1][0]])
+    .range([0, width]);
+var axisX = d3.axisBottom(x);
 
-var axisX = d3.axisBottom(scaleX);
+var line = d3.line().x((t) => x(t[0])).y((t) => scaleY(t[1]));
+
+var scaleY = d3.scaleLinear().domain([0, 10]).range([height, 0]);
 var axisY = d3.axisLeft(scaleY);
+
+chart = chart.append('g').attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 chart.append('g')
     .attr('class', 'x axis')
-    .attr('transform', `translate(${margin.left}, ${margin.top + height})`)
+    .attr('transform', `translate(0, ${height})`)
     .call(axisX);
+
 chart.append('g')
     .attr('class', 'y axis')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`)
     .call(axisY);
 
+chart.append("path")
+    .attr("class", "line")
+    .datum(data)
+    .attr("d", line);
+
+d3.selectAll("#chart").call(d3.drag().on('start', () => console.log('started')));
